2d1d5ca98156a40bb866d0a8c5378a505acbe829
madhuriSo <madhuri.padule@gmail.com>
2021-10-23 21:35:11 -0700

    Adding project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CartComponent,
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
